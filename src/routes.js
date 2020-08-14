f68c00fe966b4bb4a9d38a349a6bc771ad7e3a8f
dcl67 <danny.lopez1094@gmail.com>
2020-08-14 18:14:07 -0400

    Add new links and pages to routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,9 @@
 import BlogContent from "views/BlogContent";
+import Portraits from "views/Portraits";
+import Landscapes from "views/Landscapes";
+import Street from "views/Street";
+import aboutMe from "views/aboutMe";
+import contact from "views/contact";
 
 var routes = [
   {
@@ -13,5 +18,41 @@ var routes = [
     component: BlogContent,
     layout: ""
   },
+  {
+    path: "/blog",
+    name: "Home",
+    component: BlogContent,
+    layout: ""
+  },
+  {
+    path: "/portraits",
+    name: "Portraits",
+    component: Portraits,
+    layout: ""
+  },
+  {
+    path: "/landscapes",
+    name: "Landscapes",
+    component: Landscapes,
+    layout: ""
+  },
+  {
+    path: "/street",
+    name: "Street",
+    component: Street,
+    layout: ""
+  },
+  {
+    path: "/about-me",
+    name: "About me",
+    component: aboutMe,
+    layout: ""
+  },
+  {
+    path:"/contact",
+    name: "Contact",
+    component: contact,
+    layout: ""
+  }
 ];
 export default routes;
