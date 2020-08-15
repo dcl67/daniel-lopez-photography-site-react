17999383d49071e7c53a12cc37702478d802a0b5
dcl67 <danny.lopez1094@gmail.com>
2020-08-15 01:48:16 -0400

    Clean up unused imports from aboutMe

--- src/views/aboutMe.jsx
+++ src/views/aboutMe.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import Card from "components/Card/Card";
-import CardHeader from "components/Card/CardHeader";
-import CardBody from "components/Card/CardBody";
 
 
 export default class aboutMe extends React.Component {
