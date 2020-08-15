44533630408a3826ef6ac365c945df6e87cc207d
dcl67 <danny.lopez1094@gmail.com>
2020-08-15 01:33:16 -0400

    Add Street view file

---
+++ src/views/Street.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Card from "components/Card/Card";
+import CardHeader from "components/Card/CardHeader";
+import CardBody from "components/Card/CardBody";
+import image1 from "assets/img/street/street_1.jpg";
+import image4 from "assets/img/street/street_4.jpg";
+import image5 from "assets/img/street/street_5.jpg";
+import image6 from "assets/img/street/street_6.jpg";
+import image7 from "assets/img/street/street_7.jpg";
+
+
+export default class Street extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Card>
+        <CardHeader>
+          Street <br/>
+          <p style={{marginLeft: `0.4em`}}>
+          “Seeing is not enough; you have to feel what you photograph” – Andre Kertesz          </p>
+        </CardHeader>
+        <CardBody>
+          <img src={image1} style={{zoom: `9%`}}/>
+          <img src={image4} style={{zoom: `9%`}}/>
+          <img src={image5} style={{zoom: `9%`}}/>
+          <img src={image6} style={{zoom: `9%`}}/>
+          <img src={image7} style={{zoom: `9%`}}/>
+        </CardBody>
+        </Card>
+      </div>
+    )
+  }
+}
